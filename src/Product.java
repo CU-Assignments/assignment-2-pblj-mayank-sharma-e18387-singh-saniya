0cc5e182ae443a67d9d6a4a7ee78e96065e9f326
singh-saniya <110440533+singh-saniya@users.noreply.github.com>
2025-04-16 15:43:50 +0530

    Update and rename Product to Product.java

---
+++ src/Product.java
@@ -0,0 +1,27 @@
+
+public class Product {
+    private int id;
+    private String name;
+    private double price;
+
+    // Constructor
+    public Product(int id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    // Method to display product details
+    public void displayDetails() {
+        System.out.println("Product Details:");
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Price: " + price);
+    }
+
+    // Main method
+    public static void main(String[] args) {
+        Product product = new Product(101, "Laptop", 75000);
+        product.displayDetails();
+    }
+}
